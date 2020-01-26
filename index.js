c65840fb3f458df1ae1381f4795485b8c383b324
GSCrawley <gscrawley@gmail.com>
2020-01-26 14:55:51 -0800

    figured out how to get complementary colors from random rbg colors but when using rgbComplementary as backgroundColor it shows a blank screen and i have no idea why

--- index.js
+++ index.js
@@ -1,20 +1,116 @@
+/* eslint-disable no-mixed-operators */
+
 /* eslint-disable brace-style */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-alert */
 /* eslint-disable vars-on-top */
 
-// eslint-disable-next-line no-unused-vars
-function randomBrickColor() {
+function getRandomColor() {
   // eslint-disable-next-line func-names
-  const r = function () { return Math.floor(Math.random() * 246); };
+  let r = function () { return Math.floor(Math.random() * 256); };
+
   return `rgb(${r()},${r()},${r()})`;
 }
 
-function getRandomColor() {
-  // eslint-disable-next-line func-names
-  const r = function () { return Math.floor(Math.random() * 256); };
-  return `rgb(${r()},${r()},${r()})`;
+function componentToHex(c) {
+  const hex = c.toString(16);
+  return hex.length === 1 ? `0${hex}` : hex;
 }
+
+function hexToRgb(hex) {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16),
+  } : null;
+}
+
+function rgbComplimentary(r,g,b){
+
+  var hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  var rgb = 'rgb(' + (hex = hex.replace('#', '')).match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16); }).join(',') + ')';
+
+  // Get array of RGB values
+  rgb = rgb.replace(/[^\d,]/g, '').split(',');
+
+  var r = rgb[0]/255.0, g = rgb[1]/255.0, b = rgb[2]/255.0;
+
+  var max = Math.max(r, g, b);
+  var min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2.0;
+
+  if (max === min) {
+    h = s = 0; //achromatic
+  } else {
+    var d = max - min;
+    s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
+
+    if (max === r && g >= b) {
+      h = 1.0472 * (g - b) / d ;
+    } else if (max === r && g < b) {
+      h = 1.0472 * (g - b) / d + 6.2832;
+    } else if (max === g) {
+      h = 1.0472 * (b - r) / d + 2.0944;
+    } else if (max === b) {
+      h = 1.0472 * (r - g) / d + 4.1888;
+    }
+  }
+
+  h = h / 6.2832 * 360.0 + 0;
+
+  // Shift hue to opposite side of wheel and convert to [0-1] value
+  h += 180;
+  if (h > 360) { h -= 360; }
+  h /= 360;
+
+  // Convert h s and l values into r g and b values
+  // Adapted from answer by Mohsen http://stackoverflow.com/a/9493060/4939630
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    var hue2rgb = function hue2rgb(p, q, t) {
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1 / 6) return p + (q - p) * 6 * t;
+      if(t < 1 / 2) return q;
+      if(t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  r = Math.round(r * 255);
+  g = Math.round(g * 255); 
+  b = Math.round(b * 255);
+
+  // Convert r b and g values to hex
+  rgb = b | (g << 8) | (r << 16); 
+  return hexToRgb("#" + (0x1000000 | rgb).toString(16).substring(1));
+}
+console.log(rgbComplimentary);
+
+
+// function getComplementColor(r) {
+//   return getHexColor(0xffffff - r);
+// }
+
+// function getHexColor(r) {
+//   return `#${(`000000${r.toString(16)}`).substr(-6)}`;
+// }
+
+// console.log('--------------------------');
+// const color = getRandomColor();
+// console.log(color);
+// const compliment = getComplementColor();
+// console.log(compliment);
+
 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
@@ -34,6 +130,11 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 let ballColor = getRandomColor();
+// let backgroundColor = rgbComplimentary();
+
+console.log('>>>>', ballColor);
+// console.log('>>>>', backgroundColor);
+
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
@@ -57,7 +158,7 @@ function drawBall() {
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = '#0095DD';
+  ctx.fillStyle = 'black';
   ctx.fill();
   ctx.closePath();
 }
