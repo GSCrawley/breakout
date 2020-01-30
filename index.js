28957d747f17ff36c74a26a56a3727b5269a100e
GSCrawley <gscrawley@gmail.com>
2020-01-29 19:53:37 -0800

    trying to fill background with complimemtary color

--- index.js
+++ index.js
@@ -12,106 +12,105 @@ function getRandomColor() {
   return `rgb(${r()},${r()},${r()})`;
 }
 
-// function componentToHex(c) {
-//   const hex = c.toString(16);
-//   return hex.length === 1 ? `0${hex}` : hex;
-// }
+function parseRgb(s) {
+  s = s.slice(4, s.length-1);
+  let comp = s.split(",");// [34,50,67]
+  let result = [];
+  comp.forEach(a => {
+    result.push(parseInt(a, 10));
+  });
+  return result;
+}
 
-// function hexToRgb(hex) {
-//   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-//   return result ? {
-//     r: parseInt(result[1], 16),
-//     g: parseInt(result[2], 16),
-//     b: parseInt(result[3], 16),
-//   } : null;
-// }
+function hexToRgb(hex) {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16),
+  } : null;
+}
 
-// function rgbComplimentary(r, g, b) {
-//   let hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-//   let rgb = 'rgb(' + (hex = hex.replace('#', '')).match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16); }).join(',') + ')';
+function rgbComplimentary(r, g, b) {
+  let r2 = r/255.0, g2 = g/255.0, b2 = b/255.0;
 
-//   // Get array of RGB values
-//   rgb = rgb.replace(/[^\d,]/g, '').split(',');
+  let max = Math.max(r2, g2, b2);
+  let min = Math.min(r2, g2, b2);
+  let h, s, l = (max + min) / 2.0;
 
-//   let r2 = rgb[0]/255.0, g2 = rgb[1]/255.0, b2 = rgb[2]/255.0;
+  if (max === min) {
+    h = s = 0; //achromatic
+  } else {
+    var d = max - min;
+    s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
 
-//   let max = Math.max(r2, g2, b2);
-//   let min = Math.min(r2, g2, b2);
-//   let h, s, l = (max + min) / 2.0;
+    if (max === r2 && g2 >= b2) {
+      h = 1.0472 * (g2 - b2) / d;
+    } else if (max === r2 && g2 < b2) {
+      h = 1.0472 * (g2 - b2) / d + 6.2832;
+    } else if (max === g2) {
+      h = 1.0472 * (b2 - r2) / d + 2.0944;
+    } else if (max === b) {
+      h = 1.0472 * (r2 - g2) / d + 4.1888;
+    }
+  }
 
-//   if (max === min) {
-//     h = s = 0; //achromatic
-//   } else {
-//     var d = max - min;
-//     s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
+  h = h / 6.2832 * 360.0 + 0;
 
-//     if (max === r2 && g2 >= b2) {
-//       h = 1.0472 * (g2 - b2) / d;
-//     } else if (max === r2 && g2 < b2) {
-//       h = 1.0472 * (g2 - b2) / d + 6.2832;
-//     } else if (max === g2) {
-//       h = 1.0472 * (b2 - r2) / d + 2.0944;
-//     } else if (max === b) {
-//       h = 1.0472 * (r2 - g2) / d + 4.1888;
-//     }
-//   }
+  // Shift hue to opposite side of wheel and convert to [0-1] value
+  h += 180;
+  if (h > 360) { h -= 360; }
+  h /= 360;
 
-//   h = h / 6.2832 * 360.0 + 0;
+  // Convert h s and l values into r g and b values
+  // Adapted from answer by Mohsen http://stackoverflow.com/a/9493060/4939630
+  if (s === 0) {
+    r2 = g2 = b2 = l; // achromatic
+  } else {
+    var hue2rgb = function hue2rgb(p, q, t) {
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1 / 6) return p + (q - p) * 6 * t;
+      if(t < 1 / 2) return q;
+      if(t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
 
-//   // Shift hue to opposite side of wheel and convert to [0-1] value
-//   h += 180;
-//   if (h > 360) { h -= 360; }
-//   h /= 360;
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
 
-//   // Convert h s and l values into r g and b values
-//   // Adapted from answer by Mohsen http://stackoverflow.com/a/9493060/4939630
-//   if (s === 0) {
-//     r2 = g2 = b2 = l; // achromatic
-//   } else {
-//     var hue2rgb = function hue2rgb(p, q, t) {
-//       if(t < 0) t += 1;
-//       if(t > 1) t -= 1;
-//       if(t < 1 / 6) return p + (q - p) * 6 * t;
-//       if(t < 1 / 2) return q;
-//       if(t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-//       return p;
-//     };
+    r2 = hue2rgb(p, q, h + 1 / 3);
+    g2 = hue2rgb(p, q, h);
+    b2 = hue2rgb(p, q, h - 1 / 3);
+  }
 
-//     var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-//     var p = 2 * l - q;
+  r2 = Math.round(r2 * 255);
+  g2 = Math.round(g * 255);
+  b2 = Math.round(b * 255);
 
-//     r2 = hue2rgb(p, q, h + 1 / 3);
-//     g2 = hue2rgb(p, q, h);
-//     b2 = hue2rgb(p, q, h - 1 / 3);
-//   }
-
-//   r2 = Math.round(r2 * 255);
-//   g2 = Math.round(g * 255);
-//   b2 = Math.round(b * 255);
-
-//   // Convert r b and g values to hex
-//   rgb = b2 | (g2 << 8) | (r2 << 16);
-//   return hexToRgb("#" + (0x1000000 | rgb).toString(16).substring(1));
-// }
-// console.log('>>>> 0 >>>>', rgbComplimentary);
+  // Convert r b and g values to hex
+  rgb = b2 | (g2 << 8) | (r2 << 16);
+  return hexToRgb("#" + (0x1000000 | rgb).toString(16).substring(1));
+}
+console.log('>>>> 0 >>>>', rgbComplimentary);
 
 
-// console.log('--------------------------');
-// const color = getRandomColor();
-// console.log(color);
-// const compliment = getComplementColor();
-// console.log(compliment);
+console.log('--------------------------');
+const color = getRandomColor();
+console.log(color);
+// const compliment = rgbComplimentary(100,150,150);
+// console.log("***", compliment);
 
 // step 1 - define values //
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 
-// const ballRadius = 10;
-// let x = canvas.width / 2;
-// let y = canvas.height - 30;
-// let dx = 2;
-// let dy = -2;
+const ballRadius = 10;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
 
 const ball = {
   x: canvas.width / 2,
@@ -166,7 +165,7 @@ const game = {
 // -------- Game ----------
 
 let ballColor = getRandomColor();
-// let backgroundColor = rgbComplimentary();
+let backgroundColor = drawBackground();
 
 console.log('>>> 1 >>>', ballColor);
 // console.log('>>>>', backgroundColor);
@@ -220,27 +219,30 @@ function drawBricks() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBackground();
   drawBall();
   drawPaddle();
   collisionDetection();
   drawBricks();
   drawScore();
   drawLives();
-  // drawBackground();
   ball.x += ball.dx;
   ball.y += ball.dy;
 
   if (ball.x + ball.dx > canvas.width - ball.radius || ball.x + ball.dx < ball.radius) {
     ball.dx = -ball.dx;
     ballColor = getRandomColor();
+    backgroundColor = drawBackground();
   }
   if (ball.y + ball.dy < ball.radius) {
     ball.dy = -ball.dy;
     ballColor = getRandomColor();
+    backgroundColor = drawBackground();
   } else if (ball.y + ball.dy > canvas.height - ball.radius) {
     if (ball.x > paddleX && ball.x < paddleX + paddleWidth) {
       ball.dy = -ball.dy;
       ballColor = getRandomColor();
+      backgroundColor = drawBackground();
     } else {
       lives -= 1;
       if (!lives) {
@@ -302,11 +304,12 @@ function collisionDetection() {
     for (let r = 0; r < brickRowCount; r += 1) {
       const b = bricks[c][r];
       if (b.status === 1) {
-        if (ball.x > b.x && ball.x < b.x + brickWidth && ball.y > b.y && ball.y < b.y + brickHeight) {
+        if (ball.x > b.x && ball.x < b.x + brickWidth && ball.y > b.y && ball.y < b.y + brickHeight) 
+        {
           ball.dy = -ball.dy;
           b.status = 0;
           ballColor = getRandomColor();
-          // backgroundColor = rgbComplimentary();
+          backgroundColor = drawBackground();
           score += 1;
           if (score === brickRowCount * brickColumnCount) {
             alert('YOU WIN, CONGRATULATIONS!');
@@ -318,9 +321,12 @@ function collisionDetection() {
   }
 }
 
-// function drawBackground() {
-//   ctx.backgroundColor = 
-// }
+function drawBackground() {
+  let rgb_value = parseRgb(ballColor);
+  let compliment = rgbComplimentary(rgb_value[0], rgb_value[1], rgb_value[2]);
+  ctx.fillStyle = compliment;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 function drawScore() {
   ctx.font = '16px Arial';
